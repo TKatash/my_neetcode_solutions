9fe6eaba52e9b5a575b865f3e9ca594d62dcc223
TKatash <tkatashev@gmail.com>
2026-03-11 22:17:50 +0700

    binary search

---
+++ binary_search/binary_search.py
@@ -0,0 +1,19 @@
+from typing import List
+
+nums = [-1, 0, 2, 4, 6, 8]
+target = 4
+
+
+def search(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        if nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
